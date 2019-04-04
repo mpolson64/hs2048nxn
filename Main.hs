bffc6c855046ac8903b5868c78695801ec0aa5c0
Miles Olson <mpolson64@gmail.com>
2019-04-04 13:59:47 -0500

    Data types and a pretty print

--- Main.hs
+++ Main.hs
@@ -1,4 +1,38 @@
 module Main where
 
+import Data.List
+import System.Random
+
+data Board = Board [[Int]]
+data Gamestate = Gamestate Board StdGen
+data Direction = Up | Down | Left | Right
+
+mkBoard :: Int -> Board
+mkBoard n = Board $ replicate n $ replicate n 0
+
+showNoZero :: Int -> String
+showNoZero 0 = " "
+showNoZero n = show n
+
+pad :: Int -> String -> String
+pad n str
+    | length str < n = ' ' : pad (n - 1) str
+    | otherwise = str
+
+formatLine :: Int -> [Int] -> String
+formatLine padLength xs = "[" ++ (unwords $ map (pad padLength) (map showNoZero xs)) ++ "]"
+
+formatLines :: [[Int]] -> [String]
+formatLines board = map (formatLine (length $ show $ maximum $ map maximum board)) board 
+
+instance Show Board where
+    show (Board board) = show $ intercalate "\n" (formatLines board)
+
+getBoard :: Gamestate -> Board
+getBoard (Gamestate board _) = board
+
+getStdGen :: Gamestate -> StdGen
+getStdGen (Gamestate _ stdGen) = stdGen
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
